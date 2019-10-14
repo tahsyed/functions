99f6b466f22bf19f4b529c15f7eea8b1c743d293
tahsyed <50955483+tahsyed@users.noreply.github.com>
2019-10-14 10:26:49 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,96 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as express from 'express';
+import * as bodyParser from "body-parser";
+
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore(); // Add this
+
+const app = express();
+const main = express();
+
+main.use('/api/v1', app);
+main.use(bodyParser.json());
+
+export const webApi = functions.https.onRequest(main);
+
+app.get('/warmup', (request, response) => {
+
+    response.send('Warming up friend.');
+
+});
+
+app.post('/fights', async (request, response) => {
+  try {
+    const { winner, losser, title } = request.body;
+    const data = {
+      winner,
+      losser,
+      title
+    } 
+    const fightRef = await db.collection('fights').add(data);
+    const fight = await fightRef.get();
+
+    response.json({
+      id: fightRef.id,
+      data: fight.data()
+    });
+
+  } catch(error){
+
+    response.status(500).send(error);
+
+  }
+});
+
+app.get('/getcustomers/:id', async (request, response) => {
+  try {
+    const custId = request.params.id;
+
+    if (!custId) throw new Error('cust ID is required');
+
+    const cust = await db.collection('customers').doc(custId).get();
+	
+	
+
+    if (!cust.exists){
+        throw new Error('customer doesnt exist.')
+    }
+
+    response.json({
+      id: cust.id,
+      data: cust.data()
+    });
+
+  } catch(error){
+
+    response.status(500).send(error);
+
+  }
+});
+
+app.get('/getcustomers2/:id', async (request, response) => {
+  try {
+    const custId = request.params.id;
+
+    if (!custId) throw new Error('customer ID is required');
+
+    const cust = await db.collection('customers').doc(custId).get();
+	
+	 response.json({
+      id: cust.id,
+      title: cust.get('name')
+    });
+	
+
+
+ 
+
+  } catch(error){
+
+    response.status(500).send(error);
+
+  }
+});
+
+
